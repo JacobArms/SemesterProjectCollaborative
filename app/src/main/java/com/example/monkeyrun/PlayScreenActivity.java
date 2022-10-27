661919ff1b68053e40a6be2288e0f98798f79710
Alex-Sauer <ajsauer333@gmail.com>
2022-10-26 23:28:38 -0500

    Head Bar Removed

--- app/src/main/java/com/example/monkeyrun/PlayScreenActivity.java
+++ app/src/main/java/com/example/monkeyrun/PlayScreenActivity.java
@@ -3,12 +3,14 @@ package com.example.monkeyrun;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class PlayScreenActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_play_screen);
+        setContentView(R.layout.activity_game_end);
     }
 }
