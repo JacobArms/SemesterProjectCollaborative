ba806c78f861267f3a8aa8b613b751bd3a2ae6f3
JacobArms <90772750+JacobArms@users.noreply.github.com>
2022-10-27 08:11:49 -0500

    Working on the animation for the opening screen

--- app/src/main/java/com/example/monkeyrun/OpeningScreenActivity.java
+++ app/src/main/java/com/example/monkeyrun/OpeningScreenActivity.java
@@ -12,12 +12,19 @@ public class OpeningScreenActivity extends AppCompatActivity {
     public void imageClick(View view){
         //Grabs the image of AiAi
         ImageView imageView = (ImageView) findViewById(R.id.AiAi);
-        //Sets the xFrom, xTo, yFrom, and yTo
-        TranslateAnimation animation = new TranslateAnimation(-160.0f,160.0f, 0.0f, 0.0f);
-        animation.setDuration(800);    //animation duration, impacts the speed of the animation as well
-        animation.setRepeatCount(5);    //Sets the repeat count
-        animation.setRepeatMode(2);     //Repeat animation from (left to right, right to left)
-        imageView.startAnimation(animation);    //Starts the animation of the image on the page
+
+
+        if(imageView.getY()>-1479){
+            //Sets the xFrom, xTo, yFrom, and yTo
+            TranslateAnimation animation = new TranslateAnimation(0f,0f, 0f, -1480.0f);
+            animation.setDuration(800);    //animation duration, impacts the speed of the animation as well
+            animation.setRepeatCount(0);    //Sets the repeat count
+            animation.setRepeatMode(0);     //Repeat animation from (left to right, right to left)
+            imageView.startAnimation(animation);    //Starts the animation of the image on the page
+        }else if(imageView.getY()<= -1479){
+            imageView.setY(-1480);
+        }
+
     }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
